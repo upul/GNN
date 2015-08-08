4c9604c54c4027c57abf05993ec9a76f6b027f84
Upul Bandara <upulbandara@gmail.com>
2015-08-08 21:40:35 +0530

    added few latex scripts for chapter 2

---
+++ chapter2/simple_gradient_descent.py
@@ -0,0 +1,15 @@
+def get_grad(x):
+    """ This method returns the derivative of f(x)=x^2 function"""
+    return 2*x
+
+#initial guess
+x = 10
+#learning reate
+eta = 0.01
+
+num_iterations = 500
+for i in range(num_iterations):
+    x = x - eta*get_grad(x)
+    if i % 50 == 0:
+        print('Iteration: {:3d} x: {:.3e} f(x): {:.3e}'.format(i, x, x**2))
+print('Iteration: {:3d} x: {:.3e} f(x): {:.3e}'.format(i, x, x**2))
