64646cc70579636c4692c85c2be186b6c2f9c58d
Upul Bandara <upulbandara@gmail.com>
2015-11-08 17:26:12 +0530

    updated according to lib changes

--- chapter3/softmaxclassifier.py
+++ chapter3/softmaxclassifier.py
@@ -1,6 +1,4 @@
-import sys
 import numpy as np
-
 from layers import SoftmaxLayer
 from datareader import load_mnist
 from constants import *
@@ -10,34 +8,35 @@ x_train = x_train.reshape(MNIST_NUM_TRAINING, MNIST_NUM_FEATURES)
 y_train = y_train.reshape(MNIST_NUM_TRAINING)
 
 # initialize parameters randomly
-W = 0.1 * np.random.randn(MNIST_NUM_FEATURES, MNIST_NUM_OUTPUT)
+W = 0.001 * np.random.randn(MNIST_NUM_FEATURES, MNIST_NUM_OUTPUT)
 b = np.zeros((1, MNIST_NUM_OUTPUT))
 
-learning_rate = 0.1 # step size of the gradient descent algorithm
+learning_rate = 0.1  # step size of the gradient descent algorithm
 reg_parameter = 0.01  # regularization strength
-softmax = SoftmaxLayer(W, b, reg_parameter, MNIST_NUM_OUTPUT)
+softmax = SoftmaxLayer()
 
 num_iter = 1000
 BATCH_SIZE = 500
+
+
 for i in range(num_iter):
 
     idx = np.random.choice(MNIST_NUM_TRAINING, BATCH_SIZE, replace=True)
     x_batch = x_train[idx, :]
     y_batch = y_train[idx]
-    output_prob, loss = softmax.forward_pass(x_batch, y_batch)
+    output_prob, loss = softmax.forward_pass(x_batch, y_batch, W, b, reg_parameter, [])
     if i % 50 == 0:
         print('iteration: {:3d} loss: {:3e}'.format(i, loss))
-    gradW, gradB = softmax.backward_pass(output_prob, x_batch, y_batch)
-    W = W - learning_rate*gradW
-    b = b - learning_rate*gradB
-    softmax.update_parameters(W, b)
+    gradW, gradB, _ = softmax.backward_pass(output_prob, x_batch, y_batch, W, b, reg_parameter)
+    W = W - learning_rate * gradW
+    b = b - learning_rate * gradB
 
 # prediction
 pred_prob = np.dot(x_train, W) + b
 predicted_class = np.argmax(pred_prob, axis=1)
-print('-----------------------------')
-print('training setaccuracy: {:f}'.format(np.mean(predicted_class == y_train)))
-print('-----------------------------')
+print('---------------------------------------')
+print('training set accuracy: {:f}'.format(np.mean(predicted_class == y_train)))
+print('---------------------------------------')
 print('\n')
 
 x_test, y_test = load_mnist(MNIST_TESTING_X, MNIST_TESTING_y)
@@ -46,6 +45,6 @@ y_test = y_test.reshape(MNIST_NUM_TESTING)
 
 pred_prob = np.dot(x_test, W) + b
 predicted_class = np.argmax(pred_prob, axis=1)
-print('-----------------------------')
+print('---------------------------------------')
 print('training set accuracy: {:f}'.format(np.mean(predicted_class == y_test)))
-print('-----------------------------')
+print('---------------------------------------')
