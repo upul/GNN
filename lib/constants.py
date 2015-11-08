35e705cbd6c34f57f934ea6ea85982d2450281cb
Upul Bandara <upulbandara@gmail.com>
2015-11-08 17:30:30 +0530

    MINIST dataset's constants

---
+++ lib/constants.py
@@ -0,0 +1,10 @@
+MNIST_TRAINING_X = '/media/upul/d37cd1d6-a055-40ae-97e6-586334eb7420/home/upul/dataset_gnn_book/train-images.idx3-ubyte'
+MNIST_TRAINING_y = '/media/upul/d37cd1d6-a055-40ae-97e6-586334eb7420/home/upul/dataset_gnn_book/train-labels.idx1-ubyte'
+
+MNIST_TESTING_X = '/media/upul/d37cd1d6-a055-40ae-97e6-586334eb7420/home/upul/dataset_gnn_book/t10k-images.idx3-ubyte'
+MNIST_TESTING_y = '/media/upul/d37cd1d6-a055-40ae-97e6-586334eb7420/home/upul/dataset_gnn_book/t10k-labels.idx1-ubyte'
+
+MNIST_NUM_TRAINING = 60000
+MNIST_NUM_TESTING = 10000
+MNIST_NUM_FEATURES = 784
+MNIST_NUM_OUTPUT = 10
